faa18df1d9063d7097e34512a5ec8c3bc352e522
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-02-03 14:15:42 +0100

    bugfix: generate random secret key if .env is empty

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 from datetime import timedelta
+from django.core.management.utils import get_random_secret_key
 
 load_dotenv()
 
@@ -25,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
