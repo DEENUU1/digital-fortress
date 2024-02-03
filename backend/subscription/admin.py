052992a470d2e986f2dcccdcceda12f029da31e3
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-02-03 12:20:54 +0100

    feature: register models in admin.py

--- backend/subscription/admin.py
+++ backend/subscription/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Product, ProductPrice, UserSubscription
 
-# Register your models here.
+
+class ProductPriceAdmin(admin.ModelAdmin):
+    list_display = ('value', 'currency')
+    list_filter = ('currency',)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'max_project_storage', 'num_of_projects', 'is_active')
+    list_filter = ('price', 'num_of_projects', 'max_project_storage', 'is_active')
+
+
+class UserSubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'is_active', 'is_expired')
+    list_filter = ('product', 'is_active')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductPrice, ProductPriceAdmin)
+admin.site.register(UserSubscription, UserSubscriptionAdmin)
