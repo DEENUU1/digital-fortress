9417dd632ddc64700a67ab3c4132e95865a64bd5
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-02-03 02:01:20 +0100

    feature: register UserAccount in admin.py

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import UserAccount
 
-# Register your models here.
+
+class UserAccountAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'email', 'is_active', 'is_staff', 'is_superuser')
+
+
+admin.site.register(UserAccount, UserAccountAdmin)
