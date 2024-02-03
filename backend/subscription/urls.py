fac2de89539fdc025362a0c8ac69007310df1541
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-02-03 12:38:06 +0100

    feature: create ProductListView

--- backend/subscription/urls.py
+++ backend/subscription/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-# from .views import UserAccountMeAPI
+from .views import ProductListView
 
 
 urlpatterns_product = [
+    path('/', ProductListView.as_view(), name='product_list'),
     # path('me/', UserAccountMeAPI.as_view(), name='user_account_get_me'),
 
 ]
--- backend/subscription/views.py
+++ backend/subscription/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import status
+from .selectors.product import ProductSelector
+from .serializers import OutputProductPriceSerializer, OutputProductSerializer, OutputUserSubscription
 
-# Create your views here.
+
+class ProductListView(APIView):
+    permission_classes = (AllowAny, )
+
+    def get(self, request):
+        products = ProductSelector().list()
+        serializer = OutputProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
