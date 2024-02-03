44b7256e887cf97720cc98bf778283106e3238bb
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-02-03 02:17:22 +0100

    feature: create UserAccountMeGetAPI endpoint to return current user data

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,8 +1,14 @@
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path
+from .views import UserAccountMeGetAPI
 
 
-urlpatterns = [
+urlpatterns_jwt = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
+
+urlpatterns_user = [
+    path('me/', UserAccountMeGetAPI.as_view(), name='user_account_get_me'),
+
 ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from selectors.user import UserAccountSelector
 
-# Create your views here.
+
+class UserAccountMeGetAPI(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = UserAccountSelector.get_me(request.user.id)
+        return Response(user, status=status.HTTP_200_OK)
