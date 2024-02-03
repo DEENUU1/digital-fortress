c334e90569911f28e19e8858aa40b552bc73496e
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-02-03 12:27:23 +0100

    feature: create serializers for Product, ProductPrice and UserSubscription

---
+++ backend/subscription/serializers.py
@@ -0,0 +1,22 @@
+from .models import Product, ProductPrice, UserSubscription
+from rest_framework.serializers import ModelSerializer
+
+
+class OutputProductPriceSerializer(ModelSerializer):
+    class Meta:
+        model = ProductPrice
+        fields = ("id", "value", "currency")
+
+
+class OutputProductSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ("id", "name", "description", "price", "max_project_storage", "num_of_projects")
+
+
+class OutputUserSubscription(ModelSerializer):
+    product = OutputProductSerializer()
+
+    class Meta:
+        model = UserSubscription
+        fields = ("user", "product",  "is_active", "expiration_date")
