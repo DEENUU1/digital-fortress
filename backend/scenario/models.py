fd08baeca38c616a06da46d6bc68e2f188f3e16d
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-02-04 01:13:11 +0100

    feature: create Project model

--- backend/scenario/models.py
+++ backend/scenario/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from utils.model_base import BaseModel
+from user.models import UserAccount
 
-# Create your models here.
+
+class Project(BaseModel):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+    limit_storage = models.IntegerField(default=0)
+    current_storage = models.IntegerField(default=0)
+    num_of_scenarios = models.IntegerField(default=0)
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def storage_usage(self):
+        return f"{self.current_storage}/{self.limit_storage}"
