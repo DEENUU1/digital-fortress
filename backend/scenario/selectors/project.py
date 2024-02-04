49f20bba2df90921b5dc66ccd50089628c099b8e
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-02-04 02:02:06 +0100

    feature: add has_root method in ProjectSelector

--- backend/scenario/selectors/project.py
+++ backend/scenario/selectors/project.py
@@ -2,7 +2,7 @@ from typing import Dict
 
 from rest_framework.exceptions import ValidationError
 
-from scenario.models import Project
+from scenario.models import Project, Scenario
 from subscription.selectors.user_subscription import UserSubscriptionSelector
 from user.models import UserAccount
 from typing import List
@@ -15,3 +15,6 @@ class ProjectSelector:
 
     def get(self, project_slug: str) -> Project:
         return Project.objects.get(slug=project_slug)
+
+    def has_root(self, project_slug: str) -> bool:
+        return Scenario.objects.filter(project__slug=project_slug, parent_id=None).exists()
