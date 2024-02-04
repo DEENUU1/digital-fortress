8dd95d866b3e8b76ab143ac0b70469b2b60d2d47
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-02-04 01:46:25 +0100

    bugfix: delete instance.save() from create_slug signal

--- backend/scenario/signals.py
+++ backend/scenario/signals.py
@@ -5,12 +5,9 @@ from .models import Project
 
 
 @receiver(pre_save, sender=Project)
-def create_slug(sender, instance, created, **kwargs):
-    # Create slug for JobOffer object based on title and id
-    if created:
-        transformed_title = SlugTransformer.transform(instance.title)
-        instance.slug = f"{instance.id}-{transformed_title}"
-        instance.save()
+def create_slug(sender, instance, **kwargs):
+    transformed_title = SlugTransformer.transform(instance.title)
+    instance.slug = f"{instance.id}-{transformed_title}"
 
 
 pre_save.connect(create_slug, sender=Project)
