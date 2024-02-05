dfb11cd3c31749eaf14154f8c654575a28854b55
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-02-05 02:34:50 +0100

    feature: Create endpoints get, list, post, delete, put for Project model

--- backend/scenario/views.py
+++ backend/scenario/views.py
@@ -1,9 +1,48 @@
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from rest_framework.permissions import IsAuthenticated, AllowAny
-# from rest_framework import status
-# from .serializers import OutputProductSerializer
-# from .services.user_subscription import UserSubscriptionService
-# from .repository.user_subscription import UserSubscriptionRepository
-# from .services.product import ProductService
-# from .repository.product import ProductRepository
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import status
+from .serializers import (
+    OutputProjectSerializer,
+    OutputScenarioSerializer,
+    InputProjectSerializer,
+    InputScenarioSerializer
+)
+from .services import project as project_service, scenario as scenario_service
+from .repository import project as project_repo, scenario as scenario_repo
+
+
+class ProjectManagementAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    _service = project_service.ProjectService(project_repo.ProjectRepository())
+
+    def get(self, request):
+        projects = self._service.get_all()
+        serializer = OutputProjectSerializer(projects, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = InputProjectSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        projects = self._service.create(serializer.data)
+        return Response(projects, status=status.HTTP_201_CREATED)
+
+    def delete(self, request, project_id: int):
+        self._service.delete(project_id)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, project_id: int):
+        serializer = InputProjectSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        projects = self._service.update(project_id, serializer.data)
+        return Response(projects, status=status.HTTP_200_OK)
+
+
+class ProjectDetailsAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    _service = project_service.ProjectService(project_repo.ProjectRepository())
+
+    def get(self, request, project_slug: str):
+        project = self._service.get_by_slug(project_slug)
+        serializer = OutputProjectSerializer(project)
+        return Response(serializer.data, status=status.HTTP_200_OK)
