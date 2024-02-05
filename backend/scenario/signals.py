8983a6f0d589fd71392da66d024b01f60e26a813
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-02-05 16:59:05 +0100

    feature: create signal to set limit storage for a Project object

--- backend/scenario/signals.py
+++ backend/scenario/signals.py
@@ -2,6 +2,7 @@ from utils.slug_transformer import SlugTransformer
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Project
+from subscription.repository.user_subscription import UserSubscriptionRepository
 
 
 @receiver(post_save, sender=Project)
@@ -12,4 +13,13 @@ def create_slug(sender, instance, created, **kwargs):
         instance.save()
 
 
+@receiver(post_save, sender=Project)
+def set_limit_storage(sender, instance, created, **kwargs):
+    if created:
+        user_product = UserSubscriptionRepository().get_user_subscription(instance.user.id)
+        instance.limit_storage = user_product.max_project_storage
+        instance.save()
+
+
 post_save.connect(create_slug, sender=Project)
+post_save.connect(set_limit_storage, sender=Project)
