5ecf77ce05e55759b6b79dc408d1a807bbdc1de2
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-02-05 18:19:31 +0100

    feature: setup swagger

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,8 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Digital Fortress",
+      default_version='v1',
+      description="Docs...",
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
     path('admin/', admin.site.urls),
     path('api/v1/user/', include("user.urls")),
     path('api/v1/subscription/', include("subscription.urls")),
