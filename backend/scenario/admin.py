87c6b677b730846f13fa9d3c110c8587854b53de
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-02-05 15:18:45 +0100

    feature: register Project and Scenario models in admin.py

--- backend/scenario/admin.py
+++ backend/scenario/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Project, Scenario
 
-# Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'storage_usage', 'num_of_scenarios', 'user')
+    list_filter = ('user',)
+    search_fields = ('title',)
+
+
+class ScenarioAdmin(admin.ModelAdmin):
+    list_display = ('project', 'parent_id', 'user')
+    list_filter = ('user',)
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Scenario, ScenarioAdmin)
