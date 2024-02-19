6e555d19a7ac83bacbf606178c9d7ecf0bd92bbc
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-02-19 14:39:55 +0100

    feature: create function to save splited documents to pinecone

--- backend/file/embedding/pinecone.py
+++ backend/file/embedding/pinecone.py
@@ -1,5 +1,7 @@
 from langchain.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import Pinecone
+from typing import List
+from langchain.docstore.document import Document
 
 
 def get_embedding_func(openai_key: str) -> OpenAIEmbeddings:
@@ -11,3 +13,12 @@ def get_pinecone(pinecone_index: str, openai_key: str) -> Pinecone:
         index_name=pinecone_index,
         embedding=get_embedding_func(openai_key),
     )
+
+
+def save_to_pinecone(data: List[Document], pinecone_index: str, openai_key: str):
+    vector_db = Pinecone.from_documents(
+        data,
+        get_embedding_func(openai_key),
+        index_name=pinecone_index,
+    )
+    return vector_db
---
+++ backend/file/tasks.py
@@ -0,0 +1,8 @@
+from processor.split import split_files
+from embedding.pinecone import save_to_pinecone
+
+# def process_uploaded_file():
+#
+#     path = "/files/ads.txt"
+#     chunks = split_files(path)
+#     save_to_pinecone(chunks, pinecone_index, openai_key)
