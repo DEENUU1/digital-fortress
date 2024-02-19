f0e4feecc2b4f1a5a23bc54def5b1eb11a378bca
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-02-19 16:30:44 +0100

    feature: create process_uploaded_file task to split into chunks and save to pinecone

--- backend/file/embedding/pinecone.py
+++ backend/file/embedding/pinecone.py
@@ -8,17 +8,17 @@ def get_embedding_func(openai_key: str) -> OpenAIEmbeddings:
     return OpenAIEmbeddings(openai_api_key=openai_key)
 
 
-def get_pinecone(pinecone_index: str, openai_key: str) -> Pinecone:
+def get_pinecone(openai_key: str) -> Pinecone:
     return Pinecone.from_existing_index(
-        index_name=pinecone_index,
+        index_name=..., # todo add index here
         embedding=get_embedding_func(openai_key),
     )
 
 
-def save_to_pinecone(data: List[Document], pinecone_index: str, openai_key: str):
+def save_to_pinecone(data: List[Document], openai_key: str):
     vector_db = Pinecone.from_documents(
         data,
         get_embedding_func(openai_key),
-        index_name=pinecone_index,
+        index_name=..., # todo add index here
     )
     return vector_db
--- backend/file/tasks.py
+++ backend/file/tasks.py
@@ -1,8 +1,19 @@
 from processor.split import split_files
 from embedding.pinecone import save_to_pinecone
+from file.repository.file import FileRepository
+from user.repository.user import UserAccountRepository
+from celery import shared_task
 
-# def process_uploaded_file():
-#
-#     path = "/files/ads.txt"
-#     chunks = split_files(path)
-#     save_to_pinecone(chunks, pinecone_index, openai_key)
+
+@shared_task()
+def process_uploaded_file(_id: int, user_id: int):
+
+    file_repo = FileRepository()
+    file = file_repo.get_by_id(_id)
+
+    chunks = split_files(file.file)
+
+    user_repo = UserAccountRepository()
+    user = user_repo.get_by_id(user_id)
+
+    save_to_pinecone(chunks, user.openai_key)
