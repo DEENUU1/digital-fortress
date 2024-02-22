4807d804935a0d231281b037640471ac82afc299
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-02-22 02:51:15 +0100

    style: improve style for Node

--- frontend/components/projects/scenario/Node.tsx
+++ frontend/components/projects/scenario/Node.tsx
@@ -4,19 +4,21 @@ import ModalResponse from "@/components/projects/scenario/ModalResponse";
 import ModalCreateScenario from "@/components/projects/scenario/ModalCreateScenario";
 
 
-export default function Node({data}: { data: ScenarioResponse }) {
+export default function Node({ data }: { data: ScenarioResponse }) {
+  const hasResponse = !!data.response;
 
-	return (
-		<div className="p-3 ml-auto mr-auto bg-blue-300 bg-opacity-40 rounded-xl shadow-inner">
-			<div>
-				{data.response ? <p className="text-sm"><strong>AI: </strong>{data.response}</p> : null}
-				{data.user_details ? <p className="text-sm"><strong>User: </strong>{data.user_details}</p> : null}
-			</div>
+  return (
+    <div
+      className={`p-3 ml-auto mr-auto bg-blue-400 shadow-md bg-opacity-40 rounded-xl hover:bg-blue-600 hover:bg-opacity-40 ${
+        hasResponse ? 'w-auto' : 'w-[200px]'
+      }`}
+    >
+      {hasResponse && <strong className="text-sm">{data.response}</strong>}
 
-			<div className="space-x-2 mt-5">
-				<ModalResponse data={data}></ModalResponse>
-				<ModalCreateScenario parent_id={data.id} project_id={data.project}></ModalCreateScenario>
-			</div>
-		</div>
-	);
+      <div className="space-x-2 mt-5">
+        <ModalResponse data={data}></ModalResponse>
+        <ModalCreateScenario parent_id={data.id} project_id={data.project}></ModalCreateScenario>
+      </div>
+    </div>
+  );
 }
