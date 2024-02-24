9fe97e1acd28b0e28bbdbe984d026a671e72884c
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-02-24 12:52:12 +0100

    style: add gradient background for ProjectCard

--- frontend/components/projects/ProjectCard.tsx
+++ frontend/components/projects/ProjectCard.tsx
@@ -1,5 +1,6 @@
 import DeleteProjectButton from "@/components/projects/DeleteProject";
 import UpdateProject from "@/components/projects/UpdateProject";
+import {BackgroundGradient} from "@/components/ui/BackgroundGradient";
 
 export default function ProjectCard(
 	{
@@ -16,16 +17,18 @@ export default function ProjectCard(
 
 
 	return (
-		<div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-			<a href={`/projects/${id}`}>
-				<div className="px-4 py-3 w-72">
-					<p className="text-lg font-bold text-black truncate block capitalize">{title}</p>
+		<BackgroundGradient>
+			<div className="w-72 rounded-xl duration-500 hover:scale-105">
+				<a href={`/projects/${id}`}>
+					<div className="px-4 py-3 w-72">
+						<p className="text-lg font-bold text-black truncate block capitalize">{title}</p>
+					</div>
+				</a>
+				<div className="space-x-2 p-4">
+					<DeleteProjectButton projectId={id} onDeleteProjects={onDeleteProjects}/>
+					<UpdateProject currentTitle={title} projectId={id} onUpdateProject={onDeleteProjects}/>
 				</div>
-			</a>
-			<div className="space-x-2 p-4">
-				<DeleteProjectButton projectId={id} onDeleteProjects={onDeleteProjects}/>
-				<UpdateProject currentTitle={title} projectId={id} onUpdateProject={onDeleteProjects}/>
 			</div>
-		</div>
+		</BackgroundGradient>
 	)
 }
